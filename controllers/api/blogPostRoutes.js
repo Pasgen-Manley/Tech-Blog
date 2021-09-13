bd542f6c78b3f51114d783a7f640e3c4eee07f5a
Pasgen-Manley <pasgen95@gmail.com>
2021-09-13 15:55:41 +0800

    finished user and post routes

---
+++ controllers/api/blogPostRoutes.js
@@ -0,0 +1,59 @@
+const router = require('express').Router();
+const { Post } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+// Create a Blog Post
+router.get('/', withAuth, async (req, res) => {
+  try {
+    const newPost = await Post.create({
+      ...req.body,
+      user_id: req.session.user_id,
+    });
+
+    res.status(200).json(newPost);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+// Update a Blog Post
+router.put('/:id', withAuth, async (req, res) => {
+  try {
+    const [affectedRows] = await Post.update(req.body, {
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
+
+    if (affectedRows > 0) {
+      res.status(200).end();
+    } else {
+      res.status(404).end();
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Delete a Post
+router.delete('/:id', withAuth, async (req, res) => {
+  try {
+    const postData = await Post.destroy({
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      }
+    });
+    if (!postData) {
+      res.status(404).json({ message: 'No Post found with this id!' });
+      return;
+    }
+
+    res.status(200).json("Post Deleted!")
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,3 +2,8 @@ const router = require('express').Router();
 const { User, Post, Comment } = require('../models');
 const withAuth = require('../utils/auth');
 
+router.get('/', withAuth, async (req,res) => {
+  try {
+    const 
+  }
+})
