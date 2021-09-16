6bbc1517d60d3e05c8f61086d2b45f709ab31cc3
Pasgen-Manley <pasgen95@gmail.com>
2021-09-16 15:38:57 +0800

    comment script completed

---
+++ public/js/comment.js
@@ -0,0 +1,34 @@
+const addComment = async (event) => {
+  event.preventDefault();
+  const commentContent = document.querySelector('#comment-text').value.trim();
+
+  const blog_id = window.location.toString().split('/')[
+    window.location.toString().split('/').length - 1
+  ];
+  
+  console.log(blog_id)
+
+  if (commentContent) {
+    
+    const response = await fetch(`/api/comments`, {
+      method: 'POST',
+      body: JSON.stringify({ commentContent, blog_id }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    if (response.ok) {
+      document.location.replace('/');
+      
+    } else {
+      alert('Failed to create comment');
+    }
+  }
+};
+
+
+
+document
+  .querySelector('.new-comment-form')
+  .addEventListener('submit', addComment);
